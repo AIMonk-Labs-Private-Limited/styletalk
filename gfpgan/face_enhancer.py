d198268061b5b03a2a8733a175392617b05aa19c
root <root@e2e-96-79.ssdcloudindia.net>
2023-07-13 13:47:59 +0000

    Changes required to do super resolution on styletalk output

--- gfpgan/face_enhancer.py
+++ gfpgan/face_enhancer.py
@@ -91,7 +91,7 @@ def enhancer_generator_no_len(images, method='gfpgan', bg_upsampler='realesrgan'
         bg_upsampler = None
 
     # determine model paths
-    model_path = os.path.join('checkpoints/gfpgan', model_name + '.pth')
+    model_path = os.path.join('"models/styletalk/1/checkpoints/gfpgan', model_name + '.pth')
     
     if not os.path.isfile(model_path):
         model_path = os.path.join('checkpoints', model_name + '.pth')
